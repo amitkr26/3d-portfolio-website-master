1a2405acaa75d0360f8a4f52a07af39a80ae3aa3
Amit Kumar <101784484+amitkr26@users.noreply.github.com>
2024-10-17 18:10:52 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,5 +1,5 @@
 export const Bio = {
-  name: "Rishav Chanda",
+  name: "Amit Kumar",
   roles: [
     "Full Stack Developer",
     "Android Developer",
@@ -8,13 +8,13 @@ export const Bio = {
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/rishavchanda",
+  github: "https://github.com/amitkr26",
   resume:
-    "https://drive.google.com/file/d/1txFKZD3LNIXMc329jD9c995AU8LItOXK/view?usp=drive_link",
-  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-  twitter: "https://twitter.com/RishavChanda",
-  insta: "https://www.instagram.com/rishav_chanda/",
-  facebook: "https://www.facebook.com/rishav.chanda.165/",
+    "https://drive.google.com/file/d/1klB40FpWbHivGDWo-8z6GqkpjHHMYRYS/view",
+  linkedin: "https://www.linkedin.com/in/amitkr26",
+  twitter: "https://twitter.com/Amitkrr26",
+  insta: "https://www.instagram.com/amit.kr26",
+  facebook: "https://www.facebook.com/amitk26",
 };
 
 export const skills = [
